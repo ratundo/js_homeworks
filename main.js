76c8417f36f2e764707d5ac710251630a7a830a2
ratundo <125970240+ratundo@users.noreply.github.com>
2023-12-18 20:49:21 +0200

    corrected spaces

--- main.js
+++ main.js
@@ -1,52 +1,51 @@
-function getRandomIntInclusive(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1) + min);
-    };
+function getRandomIntInclusive (min, max) {
+  min = Math.ceil(min)
+  max = Math.floor(max)
+  return Math.floor(Math.random() * (max - min + 1) + min)
+};
 
+let sentence = prompt('Enter your sentence')
+const operationId = prompt('Enter operation ID: \n1. Unicode\n2. Delete symbol\n3. Replace symbol with random emoji\n4. Quantity of symbols')
+let symbolIndex
+if (operationId !== '4') {
+  symbolIndex = parseInt(prompt('Enter index of symbol'))
+}
 
-let sentence = prompt('Enter your sentence');
-let operationId = prompt('Enter operation ID: \n1. Unicode\n2. Delete symbol\n3. Replace symbol with random emoji\n4. Quantity of symbols');
-let symbolIndex;
-if (operationId !== "4") {
-    symbolIndex = parseInt(prompt('Enter index of symbol'))
+const smilesList = [
+  '😊',
+  '🌟',
+  '🐧',
+  '🎈',
+  '🌺',
+  '🚀',
+  '🍕',
+  '🎉',
+  '📚',
+  '💻'
+]
+
+let result
+
+if (operationId === '1') {
+  const charAtIndex = sentence.charAt(symbolIndex)
+  result = charAtIndex.charCodeAt(0)
+} else if (operationId === '2') {
+  result = sentence.slice(0, symbolIndex) + sentence.slice(symbolIndex + 1)
+} else if (operationId === '3') {
+  const smileIndex = getRandomIntInclusive(0, smilesList.length)
+  const smile = smilesList[smileIndex]
+  result = sentence.slice(0, symbolIndex) + smile + sentence.slice(symbolIndex + 1)
+} else if (operationId === '4') {
+  result = 0
+  sentence = sentence.toLowerCase()
+  for (let i = 0; i < sentence.length; i++) {
+    if (sentence.charAt(i) >= 'a' && sentence.charAt(i) <= 'z') {
+      result++
     }
-
-let smilesList = [
-    "😊", 
-    "🌟", 
-    "🐧", 
-    "🎈", 
-    "🌺", 
-    "🚀", 
-    "🍕", 
-    "🎉", 
-    "📚", 
-    "💻"
-    ];
-
-let result;
-
-if (operationId === "1") {
-    let charAtIndex = sentence.charAt(symbolIndex);
-    result = charAtIndex.charCodeAt(0);
-
-    } else if (operationId === "2") {
-        result = sentence.slice(0, symbolIndex) + sentence.slice(symbolIndex + 1);
-
-        } else if (operationId === "3") {
-            let smileIndex = getRandomIntInclusive(0, smilesList.length);
-            let smile = smilesList[smileIndex];
-            result = sentence.slice(0, symbolIndex) + smile + sentence.slice(symbolIndex + 1);
-
-            } else if (operationId === "4") {
-                result = 0;
-                sentence = sentence.toLowerCase()
-                for (let i = 0; i < sentence.length; i++) {
-                    if (sentence.charAt(i) >= 'a' && sentence.charAt(i) <= 'z') {
-                        result++;
-                        }
-                    }
-                }
+  }
+}
 
 console.log(result)
+console.log(sentence
+
+)
